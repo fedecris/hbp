cd218c80d5d0ea5fecd55e3d2a617a0823285645
Federico Cristina <federicocristina@gmail.com>
2022-01-06 16:24:36 -0300

    Initial commit

---
+++ src/main/java/health/hbp/HbpApplication.java
@@ -0,0 +1,13 @@
+package health.hbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/health/hbp/HbpApplicationTests.java
@@ -0,0 +1,13 @@
+package health.hbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
