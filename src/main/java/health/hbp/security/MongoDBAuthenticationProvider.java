7f9f0ff1d66a4ca37d422f5e6ee62310428a73dd
Federico Cristina <federicocristina@gmail.com>
2022-03-03 18:45:52 -0300

    Password hashing using Argon2

--- src/main/java/health/hbp/security/MongoDBAuthenticationProvider.java
+++ src/main/java/health/hbp/security/MongoDBAuthenticationProvider.java
@@ -1,5 +1,7 @@
 package health.hbp.security;
 
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import health.hbp.model.User;
 import health.hbp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +25,8 @@ public class MongoDBAuthenticationProvider implements AuthenticationProvider {
         String pass = auth.getCredentials().toString();
 
         User user = repository.findByUsername(name);
-        if (user == null || !user.getPassword().equals(pass) ) {
+        Argon2 a2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        if (user == null || !a2.verify(user.getPassword(), pass)) {
             return null;
         }
         return new UsernamePasswordAuthenticationToken(name, pass, new ArrayList<>());
@@ -32,5 +35,11 @@ public class MongoDBAuthenticationProvider implements AuthenticationProvider {
     @Override
     public boolean supports(Class<?> authentication) {
         return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+    // Hash pass creation for test-purposes only
+    public static void main(String[] args) {
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        System.out.println(argon2.hash(1, 1024, 1, "1234"));
     }
 }
