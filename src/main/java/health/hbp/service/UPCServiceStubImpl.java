8edc134711c2af54857631a644d043aeeebafab2
Federico Cristina <federicocristina@gmail.com>
2022-06-16 16:31:50 -0300

    Retrieve UPC details improved

--- src/main/java/health/hbp/service/UPCServiceStubImpl.java
+++ src/main/java/health/hbp/service/UPCServiceStubImpl.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 @Profile({"dev", "test"})
 public class UPCServiceStubImpl implements UPCService {
 
     @Override
     public String upcLookup(String upc) throws ResponseStatusException {
-        return String.format("{ %s : %s } ", "\"name\"", "\"galletitas sin sal\"");
+        try {
+            // Simulate some delay
+            TimeUnit.SECONDS.sleep(2L);
+        } catch (Exception e) {}
+        return String.format("{ %s : %s } ", "\"name\"", "\"FOOBAR Cookie\"");
     }
 }
