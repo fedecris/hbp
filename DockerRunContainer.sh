37da18a33bf73b68dfddc4f710f371f9aec61b0f
Federico Cristina <federicocristina@gmail.com>
2022-06-23 17:06:04 -0300

    Docker launcher parameterizable environment

--- DockerRunContainer.sh
+++ DockerRunContainer.sh
@@ -1,1 +1,12 @@
-docker run -p8080:8080 hbp
+if [ $# -ne 1 ]; then 
+	echo "Environment argument missing (dev, prod)"
+	exit
+fi
+
+if [ $1 != "prod" ] && [ $1 != "dev" ]; then
+	echo "Invalid option $1. Possible values: dev, prod"
+	exit
+fi
+
+docker run -p8080:8080 -e "SPRING_PROFILES_ACTIVE=$1" --name hbp_app hbp
+docker rm hbp_app
